c1dd11ee93bf28bd6a316e305d7e3278f1f4660e
Matthew Harrison-Jones <matthojo@gmail.com>
2016-06-08 12:16:50 +0100

    Improve score UI

--- src/js/panel.js
+++ src/js/panel.js
@@ -27,7 +27,7 @@ function outputResults (result) {
   var scoreOutput = document.createDocumentFragment()
 
   // Append Score
-  var scoreText = document.createTextNode('Score: ' + result.advice.score + '/100')
+  var scoreText = document.createTextNode(result.advice.score)
   scoreOutput.appendChild(scoreText)
   $score.appendChild(scoreOutput)
   colourScore($score, result.advice.score)
